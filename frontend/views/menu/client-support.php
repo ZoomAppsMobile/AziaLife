df7a323fbfdd224f6c5aa83222e4bf146d960170
Yuriy <yvmanzhela@mail.ru>
2018-07-09 17:50:04 +0600

    1

--- frontend/views/menu/client-support.php
+++ frontend/views/menu/client-support.php
@@ -1,1 +1,10 @@
-<a href="/client-support/statement">Заявления</a>
+<div class="link-anchors d-flex flex-md-row flex-column mt-4">
+    <a href="/">Главная <img src="/image/link-arrow-right.png" alt=""></a>
+    <a href="<?=\yii\helpers\Html::encode(\yii\helpers\Url::to(['/about-the-company']))?>">О компании <img src="/image/link-arrow-right.png" alt=""></a>
+    <a href="<?=\yii\helpers\Html::encode(\yii\helpers\Url::to(['/about-the-company/partners-and-customers']))?>">Партнеры и клиенты<img src="/image/link-arrow-right.png" alt=""></a>
+    <a href="">Текст</a>
+</div>
+
+<div class="rules d-flex flex-column">
+    <a href="/client-support/statement">Заявления</a>
+</div>
