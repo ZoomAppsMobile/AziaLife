93768cef16aee1a73a53280d598d7446e188116d
Yuriy <yvmanzhela@mail.ru>
2018-07-10 09:48:03 +0600

    SOAP

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -9,6 +9,7 @@ use frontend\models\ResetPasswordForm;
 use frontend\models\SignupForm;
 use yii\base\InvalidParamException;
 use yii\helpers\VarDumper;
+use yii\httpclient\Client;
 use yii\web\BadRequestHttpException;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
@@ -76,6 +77,60 @@ class SiteController extends Controller
             'banner3'=>$banner3,
             'banner4'=>$banner4,
             ]);
+    }
+
+    public function actionXml(){
+        $oClient = new \SoapClient(
+            'https://app.asia-life.kz/ws/asialife.wsdl',
+            [
+                'location' => 'http://asialife.kz/ws',
+                'login' => 'asialife_site',
+                'password' => '1q3wr2',
+            ]
+        );
+//        ini_set('soap.wsdl_cache_enabled',0);
+//        ini_set('soap.wsdl_cache_ttl',0);
+        $aResult = $oClient->authorizationWS(
+            [
+                'login' => 'asialife_site',
+                'password' => '1q3wr2',
+            ]
+        );
+//
+//        echo '<pre>' . print_r($aResult,true) . '</pre>';die;
+
+//        $client = new \GuzzleHttp\Client();
+//
+//        $response = $client->get('https://app.asia-life.kz/ws/asialife.wsdl', [
+//            'auth' => [
+//                'asialife_site',
+//                '1q3wr2'
+//            ]
+//        ]);
+//
+//        $response = $client->get('https://app.asia-life.kz/ws/asialife.wsdl/authorizationWS', [
+//            'auth' => [
+//                'asialife_site',
+//                '1q3wr2'
+//            ]
+//        ]);
+
+//        $client = new Client(['https://app.asia-life.kz/ws/asialife.wsdl']);
+//
+//        $loginResponse = $client->post('authorizationWS', [
+//            'login' => 'asialife_site',
+//            'password' => '1q3wr2',
+//        ])->send();
+//
+////        $loginResponse->cookies->get('PHPSESSID') - содержит новый идентификатор сессии
+//        VarDumper::dump($loginResponse);die;
+//        $client->post('account/profile', ['birthDate' => '10/11/1982'])
+//            ->setCookies($loginResponse->cookies) // передача файла cookie из ответа в запрос
+//            ->send();
+//
+//        VarDumper::dump($response);die;
+
+
     }
 
     public function actionLogin()
